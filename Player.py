7dc558e22d1587e0a2f364ea8c096b1df0c0ffae
Jake <44412992+805Jake@users.noreply.github.com>
2021-10-10 00:10:54 -0700

    Add files via upload

---
+++ Player.py
@@ -0,0 +1,25 @@
+
+
+class Player:
+
+    
+    maxHp = 100
+
+    def _init_(self, health, name, character):
+        self.health = health
+        self.name = name
+        self.character = character
+
+
+    def setMaxHP (self, max):
+        self.maxHP = max
+    
+    def heal (self, regen):
+        if (self.health + regen > self.maxHP):
+            self.health = self.maxHP
+        else:
+            self. health += regen
+    
+    
+
+    
